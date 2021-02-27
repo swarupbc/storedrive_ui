abd43f0907d70a56df2693cb7284029cc33bac43
swarupbhanjachowdhury <swarupbhanjachowdhury@gmail.com>
2021-02-27 15:15:16 +0530

    changes 2.0

---
+++ lib/views/login/auth.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:storedrive_ui/model/userModel.dart';
+
+class APIService {
+  Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
+    String url = 'https://api.samriddhisolutions.in/vconnect/rds/login.php';
+
+    final response = await http.post(url, body: requestModel.toJson());
+    if (response.statusCode == 200 || response.statusCode == 400) {
+      return LoginResponseModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('faild to load data');
+    }
+  }
+}
