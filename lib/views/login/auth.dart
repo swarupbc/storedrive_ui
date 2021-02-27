6af8ca79e5b61075ce200119ee221c07d7e86c4b
swarupbhanjachowdhury <swarupbhanjachowdhury@gmail.com>
2021-02-27 15:35:46 +0530

    changes

--- lib/views/login/auth.dart
+++ lib/views/login/auth.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storedrive_ui/model/userModel.dart';
 
 class APIService {
--- lib/views/login/loginScreen.dart
+++ lib/views/login/loginScreen.dart
@@ -1,10 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:storedrive_ui/model/LoginModel.dart';
 import 'package:storedrive_ui/model/userModel.dart';
-import 'package:storedrive_ui/views/Home/homeScreen.dart';
 import 'package:storedrive_ui/views/login/auth.dart';
 
 class LoginScreen extends StatefulWidget {
